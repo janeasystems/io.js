db540b9ce4e9171e01391907ef90ff7d79f98ae2
StefanStojanovic <stefan.stojanovic@janeasystems.com>
2023-10-20 16:29:03 +0200

    test: fix new `UV_THREADPOOL_SIZE` tests on `.env`

--- test/node-api/test_uv_threadpool_size/node-options.js
+++ test/node-api/test_uv_threadpool_size/node-options.js
@@ -12,11 +12,14 @@ if (process.config.variables.node_without_node_options) {
 const uvThreadPoolPath = '../../fixtures/dotenv/uv-threadpool.env';
 
 // Should update UV_THREADPOOL_SIZE
-const filePath = path.join(__dirname, `./build/${common.buildType}/test_uv_threadpool_size`);
+let filePath = path.join(__dirname, `./build/${common.buildType}/test_uv_threadpool_size`);
+if (common.isWindows) {
+  filePath = filePath.replaceAll('\\', '\\\\');
+}
 const code = `
    const { test } = require('${filePath}');
    const size = parseInt(process.env.UV_THREADPOOL_SIZE, 10);
-   require('assert').strictEqual(size, 5);
+   require('assert').strictEqual(size, 4);
    test(size);
  `.trim();
 const child = spawnSync(
